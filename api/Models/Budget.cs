2cbc8ac29a82325d294f439953aad8cca7c65c75
Derek <derek@core10.io>
2018-03-06 16:43:46 -0500

    [feature] budgetStarted need CONTROLLER

---
+++ api/Models/Budget.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace api.Models
+{
+    public class Budget
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [Display(Name = "Amount")]
+        public decimal Amount { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        [Display(Name = "Category")]
+        public string Category { get; set; }
+
+        [Required(ErrorMessage = "Required")]
+        public int UserId { get; set; }
+    }
+}
